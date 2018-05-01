cb72e97f06180c0dd2e1a070da3156dcc1fe305c
410321137 <33080919+410321137@users.noreply.github.com>
2018-05-01 12:07:39 +0800

    add GUI

---
+++ Assignment1/main.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import random
+import sys
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QDialog, QGraphicsView
+from mainwindow import Ui_MainWindow
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent = None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self) 
+        self.eprime = cv2.imread('C:/Users/pistori/Documents/Github/IOML2018spring/Assignment1/ImageData/Eprime.png', cv2.IMREAD_GRAYSCALE)
+        self.E = cv2.imread('C:/Users/pistori/Documents/Github/IOML2018spring/Assignment1/ImageData\E.png', cv2.IMREAD_GRAYSCALE)
+        self.I = cv2.imread('C:/Users/pistori/Documents/Github/IOML2018spring/Assignment1/ImageData\I.png', cv2.IMREAD_GRAYSCALE)
+        self.key1 = cv2.imread('C:/Users/pistori/Documents/Github/IOML2018spring/Assignment1/ImageData\key1.png', cv2.IMREAD_GRAYSCALE)
+        self.key2 = cv2.imread('C:/Users/pistori/Documents/Github/IOML2018spring/Assignment1/ImageData\key2.png', cv2.IMREAD_GRAYSCALE)
+        self.pushButton.clicked.connect(self.GDmethod)
+
+    def GDmethod(self):
+        epoch = 1
+        a = 0.00001
+        MaxIterLimit = int(self.lineEdit.text())
+        wt = np.array([random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)], np.int32)
+        ak = list()
+        ek = list()
+        IE = self.eprime.copy()
+        IE2 = self.E.copy()
+        while(epoch ==1 or epoch < MaxIterLimit):
+            for i in range (0, self.eprime.shape[0]):
+                for j in range (0, self.eprime.shape[1]):
+                    xk = np.array([self.key1[i][j], self.key2[i][j], self.I[i][j]])
+                    ak = self.res(wt, xk)
+                    ek = self.E[i][j] - ak
+                    sk = a * ek * xk
+                    wt = wt + sk
+            epoch = epoch + 1
+
+        for i in range (0, self.eprime.shape[0]):
+                for j in range (0, self.eprime.shape[1]):
+                    IE[i][j] = ( self.eprime[i][j] - wt[0] * self.key1[i][j] - wt[1] * self.key2[i][j] ) / wt[2]
+
+        for i in range (0, self.eprime.shape[0]):
+                for j in range (0, self.eprime.shape[1]):
+                    IE2[i][j] = ( self.E[i][j] - wt[0] * self.key1[i][j] - wt[1] * self.key2[i][j] ) / wt[2]
+
+        cv2.imshow('image', IE)
+        cv2.imshow('image2', IE2)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    def res(self, w, x):
+        a = w.T.dot(x)
+        return a
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+    # python .\main.py
+    # pyuic5 mainwindow.ui -o mainwindow.py
---
+++ Assignment1/mainwindow.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(267, 185)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(150, 20, 71, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit.setText("10")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(40, 20, 141, 21))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(40, 50, 61, 16))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(150, 50, 71, 20))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(150, 80, 71, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(40, 120, 181, 21))
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 267, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:12pt;\">輸入訓練次數</span></p></body></html>"))
+        self.label_2.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:12pt;\">決定ε</span></p></body></html>"))
+        self.pushButton.setText(_translate("MainWindow", "開始執行"))
+
